986a2038e0e41614c82c1989c0b84b45eddfbd4a
Nashruddin Amin <nashruddin.amin@gmail.com>
2023-02-13 11:17:33 +0700

    Improve layout

--- src/features/training-data/NewsURLInput.tsx
+++ src/features/training-data/NewsURLInput.tsx
@@ -68,7 +68,7 @@ export default function NewsURLInput() {
             <option value="negative">Negative</option>
           </select>
           <button
-            className="rounded-md border border-transparent bg-indigo-600 py-0 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="w-32 rounded-md border border-transparent bg-indigo-600 py-0 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             onClick={handleAdd}
             disabled={isSubmitting}
           >
